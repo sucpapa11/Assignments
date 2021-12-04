7c3ca3b70fd64d7058c5d484af462907cd947aee
sucpapa11 <94050779+sucpapa11@users.noreply.github.com>
2021-12-04 11:06:15 +0530

    Add files via upload

---
+++ APITesting/src/test/java/swagger/GetUserResponse.java
@@ -0,0 +1,34 @@
+package Swagger;
+
+public class GetUserResponse {
+    private int code;
+    private String type;
+    private String message;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public void printResponse(){
+        System.out.println("The Response after POST is : code: "+ getCode() + "\n" + "type: "+getType() + "\n" + "message:" +getMessage());
+    }
+}
